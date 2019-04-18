d5df8f31f2753289914eda3be33650f56898f438
tadashi-aikawa <syou.maman@gmail.com>
2019-04-19 00:00:10 +0900

    Rename to owcli

--- gtfsandbox/main.py
+++ gtfsandbox/main.py
@@ -7,13 +7,13 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(PROJECT_ROOT)
 sys.path.append(os.getcwd())
 
-import docowl
+import owcli
 
 VERSION = '0.1.0'
 
 
 def main():
-    docowl.run(cli="gtfs", version=VERSION, root='gtfsandbox')
+    owcli.run(cli="gtfs", version=VERSION, root='gtfsandbox')
 
 
 if __name__ == '__main__':
